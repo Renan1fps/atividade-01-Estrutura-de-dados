929c5cdade5077f2f4a32b8cf36fe62da8b02531
Renan1fps <renan.sergipe68@gmail.com>
2022-12-04 22:18:19 -0300

    refactor: doc shell

--- recursividade/start.sh
+++ recursividade/start.sh
@@ -3,3 +3,5 @@ echo "buildando o arquivo: $1"
 gcc $1.c -o $1.out
 echo "Em execução: $1"
 ./$1.out
+
+# para executar basta executar o start no terminal e passar o arquivo qual deseja ex: ./start.sh q1
